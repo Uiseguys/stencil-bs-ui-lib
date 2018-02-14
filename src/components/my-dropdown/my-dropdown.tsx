e1d8f0a809b9663f95dee406bcf4006a1b40e049
altrofimov <a.d.trofimov@gmail.com>
2018-02-14 18:00:10 +0300

    Fix code formatting

--- src/components/my-dropdown/my-dropdown.tsx
+++ src/components/my-dropdown/my-dropdown.tsx
@@ -1,39 +1,40 @@
-import { Component, Prop, State, Event, EventEmitter, Element } from '@stencil/core';
+import {Component, Prop, State, Event, EventEmitter, Element} from '@stencil/core';
 
 @Component({
-	tag: 'my-dropdown',
-	shadow: true,
+    tag: 'my-dropdown',
+    shadow: true,
     styleUrl: '../../../node_modules/bootstrap/dist/css/bootstrap.css'
 })
 export class MyDropdown {
 
-  @State() currentValue: string;
+    @State() currentValue: string;
 
-  @Event() postValue: EventEmitter;
-  @Element()
-  element: HTMLElement;
+    @Prop() id: string;
+    @Prop() for: string;
+    @Prop() value: string;
+    @Prop() title: string;
 
-  @Prop() id: string;
-  @Prop() for: string;
-  @Prop() value: string;
-  @Prop() title: string;
+    @Event() postValue: EventEmitter;
+    @Element()
+    element: HTMLElement;
 
-  getSelectValues(event) {
-    this.currentValue = event.currentTarget.value;
-    this.postValue.emit(this.element);
-  };
+    getSelectValues(event) {
+        this.currentValue = event.currentTarget.value;
+        this.postValue.emit(this.element);
+    };
 
-	render() {
-    const parsedValue = this.value ? JSON.parse(this.value): null;
+    render() {
+        const parsedValue = this.value ? JSON.parse(this.value) : null;
 
-    return (
-      <div class="input-group col-3">
-           <select class="custom-select" id={this.id} value={this.currentValue} onClick={(event) => this.getSelectValues(event)}>
-             {parsedValue && parsedValue.map((value) =>
-               <option>{value}</option>
-             )}
-           </select>
-      </div>
-		);
-	}
+        return (
+            <div class="input-group col-3">
+                <select class="custom-select" id={this.id} value={this.currentValue}
+                        onClick={(event) => this.getSelectValues(event)}>
+                    {parsedValue && parsedValue.map((value) =>
+                        <option>{value}</option>
+                    )}
+                </select>
+            </div>
+        );
+    }
 }
