8c39da957d53387d56a1382bda0bb4fe3799b065
gafilipoie <marius.afilipoie@gmail.com>
2018-02-16 23:33:20 +0200

    added Bootstrap Modal component

--- src/pages/stencil-bootstrap-demo/stencil-bootstrap-demo.tsx
+++ src/pages/stencil-bootstrap-demo/stencil-bootstrap-demo.tsx
@@ -147,7 +147,16 @@ export class StencilBootstrapDemo {
                                     Form
                                 </stencil-route-link>
                             </li>
-
+                            <li>
+                                <stencil-route-link
+                                    url="/dialog"
+                                    urlMatch={['/dialog']}
+                                    activeClass="active"
+                                    exact={true}
+                                >
+                                    Dialog
+                                </stencil-route-link>
+                            </li>
                         </ul>
                     </div>
 
@@ -228,7 +237,12 @@ export class StencilBootstrapDemo {
                                 exact={true}
                             >
                             </stencil-route>
-
+                            <stencil-route
+                                url={['/dialog']}
+                                component="dialog-page"
+                                exact={true}
+                            >
+                            </stencil-route>
                         </stencil-router>
                     </div>
                 </div>
