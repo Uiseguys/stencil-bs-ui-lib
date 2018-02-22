2b6637ca80bc8eb90750c3c1c50f5782344e23ad
gafilipoie <marius.afilipoie@gmail.com>
2018-02-22 19:29:53 +0200

    added components to index.html within an accordion component

--- src/pages/stencil-bootstrap-demo/stencil-bootstrap-demo.tsx
+++ src/pages/stencil-bootstrap-demo/stencil-bootstrap-demo.tsx
@@ -20,8 +20,53 @@ export class StencilBootstrapDemo {
                 </div>
 
                 <div class="row">
-                    <div>
-                       <navbar-page></navbar-page>
+                    <div class="col-lg-12">
+                        <div id="accordion">
+                            <div class="card">
+                                <div class="card-header" id="navigationComponent">
+                                    <h5 class="mb-0">
+                                        <button class="btn btn-link" data-toggle="collapse" data-target="#navigationCollapse" aria-expanded="true" aria-controls="navigationCollapse">
+                                            Navigation Component
+                                        </button>
+                                    </h5>
+                                </div>
+                                <div id="navigationCollapse" class="collapse" aria-labelledby="navigationComponent" data-parent="#accordion">
+                                    <div class="card-body">
+                                        <navbar-page></navbar-page>
+                                    </div>
+                                </div>
+                            </div>
+
+                            <div class="card">
+                                <div class="card-header" id="tooltipComponent">
+                                    <h5 class="mb-0">
+                                        <button class="btn btn-link" data-toggle="collapse" data-target="#tooltipCollapse" aria-expanded="true" aria-controls="tooltipCollapse">
+                                            Tooltip Component
+                                        </button>
+                                    </h5>
+                                </div>
+                                <div id="tooltipCollapse" class="collapse" aria-labelledby="tooltipComponent" data-parent="#accordion">
+                                    <div class="card-body">
+                                        <tooltip-page></tooltip-page>
+                                    </div>
+                                </div>
+                            </div>
+
+                            <div class="card">
+                                <div class="card-header" id="modalComponent">
+                                    <h5 class="mb-0">
+                                        <button class="btn btn-link" data-toggle="collapse" data-target="#modalCollapse" aria-expanded="true" aria-controls="modalCollapse">
+                                            Modal Component
+                                        </button>
+                                    </h5>
+                                </div>
+                                <div id="modalCollapse" class="collapse" aria-labelledby="modalComponent" data-parent="#accordion">
+                                    <div class="card-body">
+                                        <modal-page></modal-page>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
                     </div>
                 </div>
             </div>
