b80becbad9781a469aeefa7d1c7ac975912ac38b
gafilipoie <marius.afilipoie@gmail.com>
2018-03-06 19:56:31 +0200

    added log

--- src/components/cwc-typeahead/cwc-typeahead.tsx
+++ src/components/cwc-typeahead/cwc-typeahead.tsx
@@ -38,6 +38,7 @@ export class CwcTypeahead {
      * Life cycle hooks
     */
     componentWillLoad() {
+        console.log('Typeahead Component Will Load');
         this.initItemsData()
     }
 
