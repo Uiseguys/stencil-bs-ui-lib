df38a2e9d3d7848120a49715621385f1b864df5e
Sergey Volodko <sergey.v.as@agiliumlabs.com>
2018-04-09 17:59:11 +0300

    #63 enhance moment component with "humanize" feature

--- src/components/cwc-moment/cwc-moment.tsx
+++ src/components/cwc-moment/cwc-moment.tsx
@@ -11,6 +11,8 @@ export class CwcMoment {
     @Prop() unit: DurationInputArg2 = 'seconds';
     @Prop() separator: string = ':';
     @Prop() format: string;
+    @Prop() diff: boolean;
+    @Prop() humanize: boolean;
 
     @State() momentNow: string;
     @State() durationFormatted: string;
@@ -33,9 +35,30 @@ export class CwcMoment {
 
         if (this.duration) {
             const durationObj: Duration = moment.duration(Number(this.duration), this.unit);
-            this.durationFormatted = durationObj.hours() + this.separator + durationObj.minutes() + this.separator + durationObj.seconds();
+            this.durationFormatted = this.padLead(durationObj.hours()) 
+                + this.separator + this.padLead(durationObj.minutes())
+                + this.separator + this.padLead(durationObj.seconds());
         } else {
-            this.momentNow = moment(this.date).format(this.format);
+            if (this.date && this.diff && this.humanize) {
+                const oneMinutesBefore = moment().add(-1, 'minutes').format();
+                const todayBegin = moment().hours(0).minute(0).seconds(0);
+                const todayEnd = moment().hours(23).minute(59).seconds(59);
+                const yesterdayBegin = moment().add(-1, 'day').hours(0).minute(0).seconds(0);
+                const yesterdayEnd = moment().add(-1, 'day').hours(23).minute(59).seconds(59);
+
+                const momentDate = moment(this.date);
+                if (momentDate.isBetween(oneMinutesBefore, moment())) {
+                    this.momentNow = 'a moment ago';
+                } else if (momentDate.isBetween(todayBegin, todayEnd)) {
+                    this.momentNow = 'today, ' + momentDate.format("hh:mm a");
+                } else if (momentDate.isBetween(yesterdayBegin, yesterdayEnd)) {
+                    this.momentNow = 'yesterday, ' + momentDate.format("hh:mm a");
+                } else {
+                    this.momentNow = momentDate.fromNow();
+                }
+            } else {
+                this.momentNow = moment(this.date).format(this.format);
+            }
         }
     }
 
@@ -46,6 +69,14 @@ export class CwcMoment {
         } else if (this.momentNow) {
             return this.momentNow;
         }
+    }
+
+    private padLead(target): string {
+        let str = String(target);
+        while (str.length < 2) {
+            str = '0' + str;
+        }
+        return str;
     }
 
     render() {
