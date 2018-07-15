8ff457d7c0aedd60a36bae724a68918a27386200
gafilipoie <marius.afilipoie@gmail.com>
2018-07-15 20:34:41 +0300

    added namespace to stencil.config and updated reference to build js file (posible fix for integration with form-generator)

--- stencil.config.js
+++ stencil.config.js
@@ -1,7 +1,7 @@
 const sass = require('@stencil/sass');
 
 exports.config = {
-  namespace: 'index',
+  namespace: 'uiLib',
   // generateDistribution: true,
   // serviceWorker: true,
   outputTargets: [{ type: 'www' }, { type: 'dist' }],
