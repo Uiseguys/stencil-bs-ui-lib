6b654a40cef994c6095adec952e16489df6d9d83
gafilipoie <marius.afilipoie@gmail.com>
2018-07-20 21:33:59 +0300

    small change

--- src/pages/combobox-page/combobox-page.tsx
+++ src/pages/combobox-page/combobox-page.tsx
@@ -8,7 +8,7 @@ export class ComboboxPage {
     render() {
         return (
             <div class="col-lg-12">
-                <cwc-combobox>
+                <cwc-combobox label="Dropdown" placeholder="Select a value">
                     <button slot="dropdown-trigger" class="btn btn-primary"
                         type="button" data-toggle="dropdown"
                         aria-haspopup="true" aria-expanded="true">
@@ -16,8 +16,8 @@ export class ComboboxPage {
                     </button>
 
                     <div slot="dropdown-menu" class="dropdown-menu">
-                        <div class="dropdown-item" data-value="Automatic" >Automatic</div>
-                        <div class="dropdown-item" data-value="Manual" >Manual</div>
+                        <div class="dropdown-item" data-value="Automatic">Automatic</div>
+                        <div class="dropdown-item" data-value="Manual">Manual</div>
                     </div>
                 </cwc-combobox>
             </div>
