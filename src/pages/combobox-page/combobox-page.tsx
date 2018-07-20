83ae092cee91c3b95dff0ccaa3bb595644bbbb4a
gafilipoie <marius.afilipoie@gmail.com>
2018-07-20 23:36:16 +0300

    updated component call

--- src/pages/combobox-page/combobox-page.tsx
+++ src/pages/combobox-page/combobox-page.tsx
@@ -6,20 +6,16 @@ import { Component } from '@stencil/core';
 
 export class ComboboxPage {
     render() {
+        const data = ["Automatic", "Manual"];
+
         return (
             <div class="col-lg-12">
-                <cwc-combobox label="Dropdown" placeholder="Select a value">
-                    <button slot="dropdown-trigger" class="btn btn-primary"
-                        type="button" data-toggle="dropdown"
-                        aria-haspopup="true" aria-expanded="true">
-                        Select
-                    </button>
-
-                    <div slot="dropdown-menu" class="dropdown-menu">
-                        <div class="dropdown-item" data-value="Automatic">Automatic</div>
-                        <div class="dropdown-item" data-value="Manual">Manual</div>
-                    </div>
-                </cwc-combobox>
+                <cwc-combobox
+                    data={data}
+                    label="Dropdown"
+                    btnText="Select"
+                    placeholder="Select a value"
+                />
             </div>
         )
     }
