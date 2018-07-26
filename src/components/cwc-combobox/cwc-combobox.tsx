98d24c6a92950bd6c8093ebc15a612ba39564b03
gafilipoie <marius.afilipoie@gmail.com>
2018-07-26 21:30:34 +0300

    added documentation and some props to combobox component

--- src/components/cwc-combobox/cwc-combobox.tsx
+++ src/components/cwc-combobox/cwc-combobox.tsx
@@ -6,12 +6,16 @@ import 'bootstrap.native/dist/bootstrap-native-v4';
     styleUrl: 'cwc-combobox.scss'
 })
 export class ComboboxComponent {
+    /* Used for form-generator component */
     @Prop() id: string;
     @Prop() value: any;
-    @Prop() data: any[] = [];
+    /* Used for form-generator component */
+    @Prop() data: Array<string|number> = [];
     @Prop() label: string;
-    @Prop() btnText: string = "Select";
+    @Prop() btnText: string;
+    @Prop() btnLeftPosition: boolean = false;
     @Prop() placeholder: string;
+    @Prop() readonly: boolean = false;
 
     @State() dropdownValue: string
 
@@ -31,37 +35,51 @@ export class ComboboxComponent {
         this.dropdownValue = this.value;
     }
 
+    renderInput() {
+        return (
+            <input
+                id={this.id}
+                type="text"
+                class="form-control"
+                value={this.dropdownValue}
+                placeholder={this.placeholder}
+                readonly={this.readonly}
+            />
+        )
+    }
+
+    renderDropDown() {
+        return (
+            <div class="input-group-prepend">
+                <cwc-dropdown>
+                    <div slot="dropdown-trigger">
+                        <div class="btn-group w-100">
+                            <button
+                                class="btn btn-outline-secondary dropdown-toggle"
+                                type="button" data-toggle="dropdown"
+                                aria-haspopup="true" aria-expanded="true">
+                                {this.btnText}
+                            </button>
+                        </div>
+                    </div>
+                    <div slot="dropdown-menu" class="dropdown-menu">
+                        {this.data.map((value) =>  (
+                            <div class="dropdown-item" data-value={value}>{value}</div>
+                        ))}
+                    </div>
+                </cwc-dropdown>
+            </div>
+        )
+    }
+
     render() {
         return (
             <div class="form-group">
                 <label class="control-label">{this.label}</label>
                 <div class="input-group mb-3">
-                    <input
-                        id={this.id}
-                        type="text"
-                        class="form-control"
-                        value={this.dropdownValue}
-                        placeholder={this.placeholder}
-                        readonly
-                    />
-                    <div class="input-group-prepend">
-                        <cwc-dropdown>
-                            <div slot="dropdown-trigger">
-                                <div class="btn-group w-100">
-                                    <button
-                                        class="btn btn-outline-secondary dropdown-toggle dropdown-toggle-split"
-                                        type="button" data-toggle="dropdown"
-                                        aria-haspopup="true" aria-expanded="true">
-                                    </button>
-                                </div>
-                            </div>
-                            <div slot="dropdown-menu" class="dropdown-menu">
-                                {this.data.map((value) =>  (
-                                    <div class="dropdown-item" data-value={value}>{value}</div>
-                                ))}
-                            </div>
-                        </cwc-dropdown>
-                    </div>
+                    {this.btnLeftPosition && this.renderDropDown()}
+                    {this.renderInput()}
+                    {!this.btnLeftPosition && this.renderDropDown()}
                 </div>
             </div>
         );
