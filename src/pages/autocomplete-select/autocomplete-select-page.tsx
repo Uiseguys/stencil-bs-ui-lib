b4ed6ec1578ebcafcfc44e251c8770ef38a315da
divsbhalala <bhalaladivs@gmail.com>
2018-08-07 12:29:45 +0530

    publish newer version 0.8.24

--- src/pages/autocomplete-select/autocomplete-select-page.tsx
+++ src/pages/autocomplete-select/autocomplete-select-page.tsx
@@ -42,66 +42,197 @@ export class AutocompleteSelectPage {
   typeaheadOnSubmit(e) {
     this.result = e.detail;
   }
-  //Listening textchange event from cec-autocomplete-select
+
+  //Listening textchange event from cwc-autocomplete-select
   @Listen('textChange')
   textChange(e) {
     this.textChangeResult = e.detail;
   }
   //End
-  render() {
-    return [
-      <h3>Simple String[] data demo!</h3>,
-      <cwc-autocomplete-select data = {this.data}/>,
-      <h6>Text Change result: {this.textChangeResult}</h6>,
 
-      <br />,
-      <br />,
-      <h3>Complex Object[] demo!</h3>,
-      <cwc-autocomplete-select
-        data={this.complex}
-        searchKey={this.searchString}
-        placeholder="Search something e.g. 'Argentina'"
-      />,
-      <br />,
-      <h4>result: </h4>,
-      <pre>{JSON.stringify(this.result, null, 2)}</pre>,
-      <br />,
-      <br />,
-      <cwc-tag text="Holla tag" />,
-      <br />,
-      <br />,
-      <cwc-tag
-        text="Holla link"
-        imgLink="../../assets/icon/favicon.ico"
-        closable={true}
-      />,
-      <br />,
-      <br />,
-      <cwc-tag text="Holla rounded tag" rounded={true} closable={true} />,
-      <br />,
-      <br />,
-      <cwc-tag
-        text="Holla rounded link"
-        imgLink="../../assets/icon/favicon.ico"
-      />,
-      <br />,
-      <br />,
-      <cwc-tag
-        text="Holla rounded img tag"
-        imgLink="../../assets/icon/favicon.ico"
-        rounded={true}
-      />,
-      <br />,
-      <br />,
-      <cwc-tag
-        text="Holla rounded img link"
-        limitTo={10}
-        imgLink="../../assets/icon/favicon.ico"
-        rounded={true}
-        closable={true}
-      />
-      // link='https://google.com'
-      // <img src=" ../../assets/icon/favicon.ico" alt="" />
-    ];
+  render() {
+    return (
+        <div>
+            <div class="container pt-4">
+                <h3>Simple String[] data demo!</h3>
+                <cwc-autocomplete-select data={this.data} />
+                <br />
+
+                <h6>Text Change result: {this.textChangeResult}</h6>
+
+                <div class="row">
+                    <div class="col-lg-12">
+                        <div class="jumbotron pt-3">
+                            <div class="row">
+                                <div class="col-lg-12">
+                                    <div class="row">
+                                        <div class="col-lg-12">
+                                            <h3>Usage </h3>
+                                            <div>
+
+
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <br />
+                                    <div class="row">
+                                        <div class="col-lg-12">
+                                            <pre><code class="lang-tsx">
+                                                {`<cwc-autocomplete-select data={['Alex', 'Alabama', 'Alaska', 'andreas', 'alexandro']}/>`}
+                                            </code></pre>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="row">
+                                <div class="col-lg-12">
+                                    <table class="table">
+                                        <thead>
+                                        <tr>
+                                            <th>Param</th>
+                                            <th>Type</th>
+                                            <th>Default</th>
+                                            <th>Description</th>
+                                        </tr>
+                                        </thead>
+                                        <tbody>
+                                        <tr>
+                                            <td>data</td>
+                                            <td><code>Array</code></td>
+                                            <td><code>[]</code></td>
+                                            <td>Data to populate in dropdown</td>
+                                        </tr>
+                                        <tr>
+                                            <td>idValue</td>
+                                            <td><code>String</code></td>
+                                            <td><code>''</code></td>
+                                            <td>This is to set the custom id if user wanted and default id will be auto generated</td>
+                                        </tr>
+                                        <tr>
+                                            <td>label</td>
+                                            <td><code>String</code></td>
+                                            <td><code>''</code></td>
+                                            <td>This is to set label of auto complete select</td>
+                                        </tr>
+                                        <tr>
+                                            <td>textChange</td>
+                                            <td><code>Event</code></td>
+                                            <td></td>
+                                            <td>Fired when the user will trying search the result from dropdown list.</td>
+                                        </tr>
+                                        <tr>
+                                            <td>typeaheadOnSubmit</td>
+                                            <td><code>Event</code></td>
+                                            <td></td>
+                                            <td>This event will fire and return the result after select the option from dropdown.</td>
+                                        </tr>
+                                        </tbody>
+                                    </table>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+
+
+            <div class="container pt-4">
+                <h3>Complex Object[] demo!</h3>
+                <cwc-autocomplete-select
+                    data={this.complex}
+                    searchKey={this.searchString}
+                />
+                <br />
+
+                <h4>result: </h4>
+                <pre>{JSON.stringify(this.result, null, 2)}</pre>
+
+                <div class="row">
+                    <div class="col-lg-12">
+                        <div class="jumbotron pt-3">
+                            <div class="row">
+                                <div class="col-lg-12">
+                                    <div class="row">
+                                        <div class="col-lg-12">
+                                            <h3>Usage </h3>
+                                            <div>
+
+
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <br />
+                                    <div class="row">
+                                        <div class="col-lg-12">
+                                            <pre><code class="lang-tsx">
+
+                                                {`<cwc-autocomplete-select searchKey="data.name" data={{ type: 'country', data: { name: 'Austria', capital: 'Vienna' } },
+{ type: 'country', data: { name: 'Australia', capital: 'Canberra' } },
+{ type: 'country', data: { name: 'Argentina', capital: 'Buenos Aires' } }}/>`}
+                                            </code></pre>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="row">
+                                <div class="col-lg-12">
+                                    <table class="table">
+                                        <thead>
+                                        <tr>
+                                            <th>Param</th>
+                                            <th>Type</th>
+                                            <th>Default</th>
+                                            <th>Description</th>
+                                        </tr>
+                                        </thead>
+                                        <tbody>
+                                        <tr>
+                                            <td>data</td>
+                                            <td><code>Array</code></td>
+                                            <td><code>[]</code></td>
+                                            <td>Data to populate in dropdown</td>
+                                        </tr>
+                                        <tr>
+                                            <td>idValue</td>
+                                            <td><code>String</code></td>
+                                            <td><code>''</code></td>
+                                            <td>This is to set the custom id if user wanted and default id will be auto generated</td>
+                                        </tr>
+                                        <tr>
+                                            <td>label</td>
+                                            <td><code>String</code></td>
+                                            <td><code>''</code></td>
+                                            <td>This is to set label of auto complete select</td>
+                                        </tr>
+                                        <tr>
+                                            <td>searchKey</td>
+                                            <td><code>String</code></td>
+                                            <td><code>''</code></td>
+                                            <td>This is to search in complex data where we can set the key to get the result</td>
+                                        </tr>
+                                        <tr>
+                                            <td>textChange</td>
+                                            <td><code>Event</code></td>
+                                            <td></td>
+                                            <td>Fired when the user will trying search the result from dropdown list.</td>
+                                        </tr>
+                                        <tr>
+                                            <td>typeaheadOnSubmit</td>
+                                            <td><code>Event</code></td>
+                                            <td></td>
+                                            <td>This event will fire and return the result after select the option from dropdown.</td>
+                                        </tr>
+                                        </tbody>
+                                    </table>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+    )
   }
 }
