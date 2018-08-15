3959264f132669c0c7576db8c81e88c8e68b9686
Vanessa <vanessaabordo@yahoo.com>
2018-08-15 17:35:46 +0800

    try different display prop name

--- src/pages/checkbox-group-page/checkbox-group-page.tsx
+++ src/pages/checkbox-group-page/checkbox-group-page.tsx
@@ -8,14 +8,14 @@ import { Component } from '@stencil/core';
 export class DropdownPage {
     render() {
         const data = [
-          {name: 'myname', key: 'mykey'},
-          {name: 'myname1', key: 'mykey1'},
-          {name: 'myname2', key: 'mykey2'},
+          {test: 'myname', key: 'mykey'},
+          {test: 'myname1', key: 'mykey1'},
+          {test: 'myname2', key: 'mykey2'},
         ];
 
         return (
           <div>
-            <cwc-checkbox-group data={data}></cwc-checkbox-group>
+            <cwc-checkbox-group displayProp='test' data={data}></cwc-checkbox-group>
           </div>
         );
     }
