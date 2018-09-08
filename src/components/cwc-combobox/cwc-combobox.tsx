213a23d12a25acc85d66e864ddabd351a80c6aee
gafilipoie <marius.afilipoie@gmail.com>
2018-09-08 23:47:46 +0300

    idented with 2 spaces, changed postValue event emitter name into onChange

--- src/components/cwc-combobox/cwc-combobox.tsx
+++ src/components/cwc-combobox/cwc-combobox.tsx
@@ -2,85 +2,85 @@ import { Component, Prop, State, Listen, Event, EventEmitter } from '@stencil/co
 import 'bootstrap.native/dist/bootstrap-native-v4';
 
 @Component({
-    tag: 'cwc-combobox',
-    styleUrl: 'cwc-combobox.scss'
+  tag: 'cwc-combobox',
+  styleUrl: 'cwc-combobox.scss'
 })
 export class ComboboxComponent {
-    @Prop() id: string;
-    @Prop() value: any;
+  @Prop() id: string;
+  @Prop() value: any;
+  @Prop() label: string;
+  @Prop() placeholder: string;
 
-    @Prop() data: Array<string|number> = [];
-    @Prop() label: string;
-    @Prop() btnText: string;
-    @Prop() btnLeftPosition: boolean = false;
-    @Prop() placeholder: string;
-    @Prop() readonly: boolean = false;
+  @Prop() data: Array<string|number> = [];
+  @Prop() btnText: string;
+  @Prop() btnLeftPosition: boolean = false;
+  @Prop() readonly: boolean = false;
 
-    @State() dropdownValue: string
+  @State() dropdownValue: string
 
-    @Listen('change')
-    dropdownHandler(value: CustomEvent): void {
-        this.dropdownValue = value.detail
-        this.postValue.emit({
-            id: this.id,
-            value: this.dropdownValue,
-            type: 'dropdown'
-        });
-    }
+  @Event() onChange: EventEmitter;
 
-    @Event() postValue: EventEmitter;
+  @Listen('change')
+  dropdownHandler(value: CustomEvent): void {
+    this.dropdownValue = value.detail
+    this.onChange.emit({
+      id: this.id,
+      value: this.dropdownValue,
+      type: 'dropdown'
+    });
+  }
 
-    componentWillLoad() {
-        this.dropdownValue = this.value;
-    }
+  componentWillLoad() {
+    this.dropdownValue = this.value;
+  }
 
-    renderInput() {
-        return (
-            <input
-                id={this.id}
-                type="text"
-                class="form-control"
-                value={this.dropdownValue}
-                placeholder={this.placeholder}
-                readonly={this.readonly}
-            />
-        )
-    }
+  renderInput() {
+    return (
+      <input
+        id={this.id}
+        type="text"
+        class="form-control"
+        value={this.dropdownValue}
+        placeholder={this.placeholder}
+        readonly={this.readonly}
+      />
+    )
+  }
 
-    renderDropDown() {
-        return (
-            <div class="input-group-prepend">
-                <cwc-dropdown>
-                    <div slot="dropdown-trigger">
-                        <div class="btn-group w-100">
-                            <button
-                                class="btn btn-outline-secondary dropdown-toggle"
-                                type="button" data-toggle="dropdown"
-                                aria-haspopup="true" aria-expanded="true">
-                                {this.btnText}
-                            </button>
-                        </div>
-                    </div>
-                    <div slot="dropdown-menu" class="dropdown-menu">
-                        {this.data.map((value) =>  (
-                            <div class="dropdown-item" data-value={value}>{value}</div>
-                        ))}
-                    </div>
-                </cwc-dropdown>
+  renderDropDown() {
+    return (
+      <div class="input-group-prepend">
+        <cwc-dropdown>
+          <div slot="dropdown-trigger">
+            <div class="btn-group w-100">
+              <button
+                  class="btn btn-outline-secondary dropdown-toggle"
+                type="button" data-toggle="dropdown"
+                aria-haspopup="true" aria-expanded="true">
+                {this.btnText}
+              </button>
             </div>
-        )
-    }
+          </div>
+          <div slot="dropdown-menu" class="dropdown-menu">
+            {this.data.map((value) =>  (
+              <div class="dropdown-item" data-value={value}>{value}</div>
+            ))}
+          </div>
+        </cwc-dropdown>
+      </div>
+    )
+  }
 
-    render() {
-        return (
-            <div class="form-group">
-                <label class="control-label">{this.label}</label>
-                <div class="input-group mb-3">
-                    {this.btnLeftPosition && this.renderDropDown()}
-                    {this.renderInput()}
-                    {!this.btnLeftPosition && this.renderDropDown()}
-                </div>
-            </div>
-        );
-    }
+  render() {
+    return (
+      <div class="form-group">
+        <label class="control-label">{this.label}</label>
+        <div class="input-group mb-3">
+          {this.btnLeftPosition && this.renderDropDown()}
+          {this.renderInput()}
+          {!this.btnLeftPosition && this.renderDropDown()}
+        </div>
+      </div>
+    );
+  }
 }
